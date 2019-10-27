5b3cc35f487f74cd2b572ffca4d7f8163e2168e4
Jeykumar Sankaran <jeykumar83@gmail.com>
2019-10-26 18:58:04 -0700

    wip: scanner integrated with UI list add

--- hello_world/platforms/android/app/src/main/assets/www/js/button.js
+++ hello_world/platforms/android/app/src/main/assets/www/js/button.js
@@ -16,73 +16,144 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
-    // Application Constructor
-    initialize: function() {        
-        //document.addEventListener('deviceready', this.onDeviceReady, false);
-        $(document).ready(this.onDeviceReady)
-        console.log("initliaze");
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {    
-        $("#my_button").click(app.onButtonClick);       
 
-        console.log("onDevice ready");
-    },
+$(document).ready(this.onDeviceReady);  
+                
+function onDeviceReady() {    
+    //$("#my_button").click(app.onButtonClick);       
+    $("#add_button").click(onAddClick);
+    $("#add_item_button").click(onItemAdd);
+    //$("#barcode_icon").click(onScanStart);
 
-    onButtonClick: function() {    
-        let camera_options = {
-            quality: 100,            
-        };
+    console.log("onDevice ready");
+}
 
-        navigator.camera.getPicture(app.onSuccess, app.onFailure, camera_options);
+function onScanStart() {
+    console.log("onScanStart");
 
-        $("#my_button").hide();
-        $('#my_button_div').hide();
+    var option = {
+        torchOn: true,
+    };
+    cordova.plugins.barcodeScanner.scan(onScanSuccess, onScanError, option);   
+}
 
-        console.log("clicked!"); 
-    },
 
-    extractText: function(imgPath) {
-        Tesseract.recognize(imgPath)
-        .progress(app.updateProgress)
-        .then(app.onResult);
-    },
+function onScanSuccess(result) {
+        console.log("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
 
-    updateProgress: function(value) {   
-        if (value.status === "recognizing text") {            
-            $("#my_progress_bar").css("width", value.progress * 100 + "%");            
-            $("#my_progress_bar").text(value.progress.toFixed(2) * 100 + "%");            
-            $("#my_progress").css("display", "flex");
+        $("#number").val(result.text);
+   }
 
-            console.log(value.progress);            
-        }
-    },
+function onScanError(error) {
+        console.log("error" + error);
+}
 
-    onResult: function(result) {
-        $("#my_progress").css("display", "none");
+function onItemClick(event) {
+    var button = event.target;
+    console.log(button.innerHTML);
+    $("#display_vendor").html(button.innerHTML);
+    $("#display_code").html(button.dataset.code);
+    
 
-        $("#my_card_body").text(result.text);
-        $("#my_card").css("display", "flex")       
+    console.log($("#display_vendor").html() + " " + $("#display_code").html());
 
-        console.log(result.text);
-        console.log("Tesseract finished!");
-    },
+    $("#displayModal").modal();
+}
 
-    onSuccess: function(imgURI) {        
-        $("#my_photo").attr("src", imgURI);
-        $("#my_photo").css("display", "block");
+function onItemAdd() {
+    console.log("onItemAdd");      
+    console.log("Scan Number:" + $("#vendor").val());                
+    var button = $("<button/>", {
+        class: "btn btn-success",
+        text: $("#vendor").val(),                    
+        css: "color: green",
+        id: "new_button",
+        click: onItemClick,
+    });
 
-        console.log("onPhoto Success!");
+    button.attr("data-code", $("#number").val());
 
-        app.extractText(imgURI);
-    },
+    $("#button_group").append(button);
+    $("#myModal").modal("hide");
+    console.log("onItem Add exit");
+}           
 
-    onFailure: function(msg) {
-        console.log("Camera getpicture failed with the error ${msg}");
-    }
-};
+function onAddClick() {
+    console.log("onAdd click");               
+    $("#myModal").modal();                
+    console.log("exiting onAdd click");
+}
+
+    // onButtonClick: function() {
+    //     var option = {
+    //         torchOn: true,
+    //     };
+
+    //     cordova.plugins.barcodeScanner.scan(app.onScanSuccess, app.onScanError, option);            
+
+    //     $("#my_button").hide();
+    //     $('#my_button_div').hide();
+
+    //     console.log("clicked!"); 
+    // },
+
+    // onScanSuccess(result) {
+    //     console.log("We got a barcode\n" +
+    //             "Result: " + result.text + "\n" +
+    //             "Format: " + result.format + "\n" +
+    //             "Cancelled: " + result.cancelled);
+
+    //             cordova.plugins.barcodeScanner.encode(cordova.plugins.barcodeScanner.Encode.TEXT_TYPE, "1101000055441998", function(success) {
+    //                 console.log("encode success: " + success);
+    //               }, function(fail) {
+    //                 console.log("encoding failed: " + fail);
+    //               }
+    //             );
+    // },
+
+    // onScanError(error) {
+    //     console.log("error" + error);
+    // },
+
+    // extractText: function(imgPath) {
+    //     Tesseract.recognize(imgPath)
+    //     .progress(app.updateProgress)
+    //     .then(app.onResult);
+    // },
+
+    // updateProgress: function(value) {   
+    //     if (value.status === "recognizing text") {            
+    //         $("#my_progress_bar").css("width", value.progress * 100 + "%");            
+    //         $("#my_progress_bar").text(value.progress.toFixed(2) * 100 + "%");            
+    //         $("#my_progress").css("display", "flex");
+
+    //         console.log(value.progress);            
+    //     }
+    // },
+
+    // onResult: function(result) {
+    //     $("#my_progress").css("display", "none");
+
+    //     $("#my_card_body").text(result.text);
+    //     $("#my_card").css("display", "flex")       
+
+    //     console.log(result.text);
+    //     console.log("Tesseract finished!");
+    // },
+
+    // onSuccess: function(imgURI) {        
+    //     $("#my_photo").attr("src", imgURI);
+    //     $("#my_photo").css("display", "block");
+
+    //     console.log("onPhoto Success!");
+
+    //     app.extractText(imgURI);
+    // },
+
+    // onFailure: function(msg) {
+    //     console.log("Camera getpicture failed with the error ${msg}");
+    // }
 
